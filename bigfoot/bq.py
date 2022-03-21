601c8625ea4b1ecfdc3afca94b20801a40eeac5c
Logan Hennessy <logan@gradient.com>
2022-03-20 21:56:51 -0700

    Add a create-table command

--- bigfoot/bq.py
+++ bigfoot/bq.py
@@ -1,4 +1,5 @@
 import typer
+from typing import Union
 
 from google.cloud import bigquery
 
@@ -12,3 +13,12 @@ def get(gcp_project: str, dataset: str, table_name: str):
     client = bigquery.Client()
     table = client.get_table(table_id)
     client.schema_to_json(table.schema, f"../data/{dataset}__{table_name}.json")
+
+
+@bq_app.command()
+def create_table(gcp_project: str, dataset: str, table_name: str):
+    client = bigquery.Client()
+    schema = client.schema_from_json(f"../data/{dataset}__{table_name}.json")
+    table_id: Union[bigquery.table.TableReference, str] = f"{gcp_project}.{dataset}.{table_name}"
+    table = bigquery.Table(table_id, schema=schema)  # noqa; PyCharm type hinting doesn't pick up doc strings
+    client.create_table(table)
